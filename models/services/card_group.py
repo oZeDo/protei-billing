950f75afb5885a5ebec0a6374cfc2c3f47dce016
oZeDo <ooooZeDoooo@gmail.com>
2021-09-16 18:45:11 +0300

    fix: add virtualGroupId attribute to model due to xgate update

--- models/services/card_group.py
+++ models/services/card_group.py
@@ -12,6 +12,7 @@ class CardGroup(BaseModelOperations):
     clientId: int = atr(converter=convert_to(int))
     name: str = atr(default=fake.name(), converter=str)
     info: str = atr(converter=convert_to(str))
+    virtualGroupId: str = atr(eq=False, converter=convert_to(str))
     id: int = atr(eq=False, converter=convert_to(int))
     parentGroupId: int = atr(converter=convert_to(int))
 
